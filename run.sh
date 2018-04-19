78145985824ba879d574e533dfe25c5a392432d4
Omar Awile <omar.awile@epfl.ch>
2018-04-19 16:17:34 +0200

    adapted run.sh to new scenario

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -x
+
+module purge
 module load intel-parallel-studio
 make clean && make
+export MKL_DYNAMIC=false
+export OMP_NESTED=true
+export OMP_PROC_BIND=spread,close
+export OMP_NUM_THREADS=2
 export MKL_NUM_THREADS=18
-export KMP_AFFINITY=compact,1,0,granularity=fine
 
-numactl -N 0 ./dgemm
-numactl -N 1 ./dgemm
+numactl ./dgemm
