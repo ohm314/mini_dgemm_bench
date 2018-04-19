089061d83bc9e31819daf73554dbd248464c0a00
Omar Awile <omar.awile@epfl.ch>
2018-04-19 15:30:26 +0200

    trying the mkl/openmp nested model for multiple numa domains

--- dgemm.cpp
+++ dgemm.cpp
@@ -1,51 +1,74 @@
 #include <iostream>
 #include <chrono>
 #include "mkl.h"
+#include "omp.h"
+
 
 #define fix_lda(x)   (((x + 255) & ~255) + 16)
 #define min(x,y) (((x) < (y)) ? (x) : (y))
 
 int main(int argc, char **argv) {
-  double *A, *B, *C;
   double alpha, beta;
   int m, n, k;
   int niter = 100;
-
+  int nthreads = 2;
   alpha = 1.0;
   beta = 1.0;
   m = 8000;
   k = fix_lda(4096);
   n = fix_lda(4096);
 
+  omp_set_max_active_levels(2);
+
   double gflop = (2.0*m*k*n)*1e-9*niter;
 
-  A = (double*)mkl_malloc(m*k*sizeof(double), 64);
-  B = (double*)mkl_malloc(k*n*sizeof(double), 64);
-  C = (double*)mkl_malloc(m*n*sizeof(double), 64);
-  if ((A == NULL) || (B == NULL) || (C == NULL)) {
-    mkl_free(A);
-    mkl_free(B);
-    mkl_free(C);
-    return 1;
+  auto As = (double**)malloc(nthreads*sizeof(double*));
+  auto Bs = (double**)malloc(nthreads*sizeof(double*));
+  auto Cs = (double**)malloc(nthreads*sizeof(double*));
+
+  for (int ithrd = 0; ithrd < nthreads; ithrd++) {
+    auto A = (double*)mkl_malloc(m*k*sizeof(double), 64);
+    auto B = (double*)mkl_malloc(k*n*sizeof(double), 64);
+    auto C = (double*)mkl_malloc(m*n*sizeof(double), 64);
+    if ((A == NULL) || (B == NULL) || (C == NULL)) {
+      mkl_free(A);
+      mkl_free(B);
+      mkl_free(C);
+      return 1;
+    }
+    As[ithrd] = A;
+    Bs[ithrd] = B;
+    Cs[ithrd] = C;
+  }
+  #pragma omp parallel for schedule(static)
+  for (int ithrd = 0; ithrd < nthreads; ithrd++) {
+    auto A = As[ithrd];
+    auto B = Bs[ithrd];
+    auto C = Cs[ithrd];
+    for(int i = 0; i < m*k; ++i) {
+      A[i] = 1.1*(i+1);
+    }
+    for(int i = 0; i < k*n; ++i) {
+      B[i] = 1.2*(i+2);
+    }
+    for(int i = 0; i < m*n; ++i) {
+      C[i] = 0.0;
+    }
   }
 
-  for(int i = 0; i < m*k; ++i) {
-    A[i] = 1.1*(i+1);
+  #pragma omp parallel for schedule(static)
+  for (int ithrd = 0; ithrd < nthreads; ithrd++) {
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                m, n, k, alpha, As[ithrd], k, Bs[ithrd], n, beta, Cs[ithrd], n);
   }
-  for(int i = 0; i < k*n; ++i) {
-    B[i] = 1.2*(i+2);
-  }
-  for(int i = 0; i < m*n; ++i) {
-    C[i] = 0.0;
-  }
-
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-              m, n, k, alpha, A, k, B, n, beta, C, n);
 
   auto tstart = std::chrono::high_resolution_clock::now();
-  for (int iter = 0; iter < niter; iter++) {
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                m, n, k, alpha, A, k, B, n, beta, C, n);
+  #pragma omp parallel for schedule(static)
+  for (int ithrd = 0; ithrd < nthreads; ithrd++) {
+    for (int iter = 0; iter < niter; iter++) {
+      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                  m, n, k, alpha, As[ithrd], k, Bs[ithrd], n, beta, Cs[ithrd], n);
+    }
   }
   auto tend = std::chrono::high_resolution_clock::now();
 
@@ -60,7 +83,6 @@ int main(int argc, char **argv) {
     }
     std::cout << std::endl;
   }
-#endif
   double chk;
   double sgn = 1.0;
   for(int i = 0; i < m*n; ++i) {
@@ -73,5 +95,6 @@ int main(int argc, char **argv) {
   mkl_free(B);
   mkl_free(C);
 
+#endif
   return 0;
 }
